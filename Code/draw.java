76197466fee64676c13729118e6d33ad35fe4498
D.Henry <1009444100@qq.com>
2019-10-22 17:41:58 +0800

    :truck: modified Code

--- Code/draw.java
+++ Code/draw.java
@@ -1,3 +1,5 @@
+package Code;
+
 import edu.princeton.cs.algs4.StdDraw;
 
 public class draw {
---
+++ Code/test.java
@@ -0,0 +1,12 @@
+package Code;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import static edu.princeton.cs.algs4.StdOut.println;
+
+
+public class test {
+    public static void main(String[] args) {
+        StdOut.println("ddd");
+    }
+}
